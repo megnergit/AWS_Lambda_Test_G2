fa2d0e1d3dfcd9021b8f1908a9e5695d4ce7aa98
Meg <miwa.egner@gmail.com>
2025-02-06 11:56:12 +0100

    Update README.md

--- lambda_test1.py
+++ lambda_test1.py
@@ -2,6 +2,7 @@ def handler(event, context):
     name = event.get("name", "Anonymous")
     weather = event.get("weather", "unknown")
 
+
     return {
         'statusCode': 200,
         'body': f"Hi {name}, hello from Lambda. It is {weather} today.",
